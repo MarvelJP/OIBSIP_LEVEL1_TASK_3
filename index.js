a1b888463ece201c4ff2259e0a30dec809afdcb7
Jay <jayhapaliya141206@gmail.com>
2022-05-19 22:52:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fehrenheit");
+
+function calToFer() {
+    let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function ferToCal() {
+    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+}
